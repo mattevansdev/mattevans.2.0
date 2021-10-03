05b54c12fcad22168f6b5e26b6283303a1deea63
Matthew Evans <m@iammatthew.com>
2021-10-03 13:51:50 -0700

    fixed about me section

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -7,12 +7,10 @@ class About extends Component {
                 <div className="jumbotron about-me px-20 sm:py-10 md:py-20 xl:py-28">
                     <div class="container max-w-8xl">
                         <h3 className="text-3xl font-bold text-center xl:text-4xl py-10 xl:py-10 ">Hello again, Nice to meet you.</h3>
-                        <p className="text-center leading-loose md:text-base xl:text-lg xl:px-48">I live in Kirkland, WA with my beautiful wife and our dogs Charlie and Eleven. When I’m not
-                            working, you’ll find me spending time with friends, eating pizza (hint: favorite food), listening to music,
-                            hiking, or traveling. I also enjoying photography.</p>
+                        <p className="text-center text-xl leading-loose xl:text-lg xl:px-48">I live in Kirkland, WA with my beautiful wife and our dogs Charlie and Eleven.</p>
                     </div>
                 </div>
-                <div className="bg-gray-100 container mx-auto skills py-3 text-gray-900 ">
+                <div className="bg-gray-100 container mx-auto rounded-3xl -mt-60 py-3 text-gray-900 ">
                     <div className="text-center grid md:col-1 xl:grid-cols-3 divide-x divide-gray-200">
                         <div className="py-10">
                             <img className="py-10 mx-auto" src="/assets/images/web-programming.png" width="64" height="auto"
