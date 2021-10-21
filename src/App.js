8f75d594b921789407c9ddb49879378e52072702
Matthew Evans <m@iammatthew.com>
2021-10-20 17:59:32 -0700

    Added About Page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import SinglePosts from './components/SinglePostComponent';
 import Projects from './components/ProjectsComponent'
 import Contact from './components/ContactComponent';
 import Work from './components/WorkComponent';
+import AboutPage from './components/AboutPageComponent';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 
@@ -24,6 +25,7 @@ class App extends Component {
             <Route component={Work} path="/" exact />
             <Route component={AllPosts} path="/blog" exact />
             <Route component={SinglePosts} path="/blog/:slug" />
+            <Route component={AboutPage} path="/about" />
             <Route component={Workspace} path="/workspace" />
             <Route component={Projects} path="/projects" />
             <Route component={Contact} path="/contact" />
