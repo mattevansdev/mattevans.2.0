9ae7c4100f9f9188209ea7a1d697d7f29bc72812
Matthew Evans <m@iammatthew.com>
2021-10-21 22:57:02 -0700

    new burger menu

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,4 +1,5 @@
 import React, { Component, useState } from 'react';
+import { slide as Menu } from 'react-burger-menu'
 import { NavLink } from 'react-router-dom';
 
 // const btn = useState("button.mobile-menu-button");
@@ -8,12 +9,7 @@ import { NavLink } from 'react-router-dom';
 //     menu.classList.toggle("hidden");
 // });
 
-
 function Header() {
-    const [navbarOpen, setNavbarOpen] = useState(false)
-    const handleToggle = () => {
-        setNavbarOpen(!navbarOpen)
-    }
     return (
         <nav className="bg-gray-50 dark:bg-gray-800">
             <div className="container">
@@ -25,10 +21,8 @@ function Header() {
                             </a>
                         </div>
                     </div>
-                    <div className="flex items-center space-x-8">
-                        <label for="menu-toggle" className="md:hidden block"><svg class="fill-current text-gray-900 dark:text-gray-300" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><title>menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path></svg></label>
-                        <input class="hidden" type="checkbox" id="menu-toggle" />
-                        <div className="hidden md:flex items-center space-x-8" id="menu">
+                    <div>
+                        <Menu right >
                             <NavLink className="text-lg md:text-xl font-bold uppercase text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-500" to="/blog">
                                 <span>Blog</span>
                             </NavLink>
@@ -38,25 +32,10 @@ function Header() {
                             <NavLink className="text-lg md:text-xl font-bold uppercase text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-500" to="/contact">
                                 <span>Contact</span>
                             </NavLink>
-                        </div>
+                        </Menu>
                     </div>
-
                 </div>
             </div>
-
-            {/* <div>
-                <div className="flex px-20 justify-between mobile md:hidden">
-                    <NavLink className="link" to="/blog">
-                        <span>Blog</span>
-                    </NavLink>
-                    <NavLink className="link" to="/projects">
-                        <span>Projects</span>
-                    </NavLink>
-                    <NavLink className="link" to="/contact">
-                        <span>Contact</span>
-                    </NavLink>
-                </div>
-            </div> */}
         </nav>
     );
 }
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -7,17 +7,15 @@ class Home extends Component {
   render() {
     return (
       <div className="bg-gray-50 dark:bg-gray-800">
-        <div className="container">
-          <div className="grid place-content-center min-h-full text-center pb-80">
-            <img className="profile" src="/assets/images/profile.jpg" alt="Matthew Evans" />
-            <div className="flex-1 max-w-7xl">
-              <h1 className="text-xl lg:text-5xl font-extrabold dark:text-gray-300">Full Stack & Mobile Application Developer</h1>
-              <p className="py-3 text-base leading-10 lg:text-2xl dark:text-gray-300">Hi, I'm Matt. In a galaxy far, far away, I write beautiful code creating successful websites and mobile applications</p>
-              <div className="flex justify-center">
-                <div className="pt-16 px-3"><NavLink className="link" to="/projects"><button class="text-lg font-bold py-2 px-4 rounded-2xl bg-gray-800 text-gray-100 dark:bg-indigo-50 dark:text-gray-800">
-              Recent Projects
-            </button></NavLink></div>
-              </div>
+        <div className="grid place-content-center text-center pb-80">
+          <img className="profile" src="/assets/images/profile.jpg" alt="Matthew Evans" />
+          <div className="flex-1 max-w-7xl">
+            <h1 className="text-xl lg:text-5xl font-extrabold dark:text-gray-300">Full Stack & Mobile Application Developer</h1>
+            <p className="py-3 text-base leading-10 lg:text-2xl dark:text-gray-300">Hi, I'm Matt. In a galaxy far, far away, I write beautiful code creating successful websites and mobile applications</p>
+            <div className="flex justify-center">
+              <div className="pt-16 px-3"><NavLink className="link" to="/projects"><button class="text-lg font-bold py-2 px-4 rounded-2xl bg-gray-800 text-gray-100 dark:bg-indigo-50 dark:text-gray-800">
+                Recent Projects
+              </button></NavLink></div>
             </div>
           </div>
         </div>
