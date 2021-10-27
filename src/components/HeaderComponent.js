f14be2d38b4106ee9e743c69c066aab4ed0d6c50
Matthew Evans <m@iammatthew.com>
2021-10-26 19:11:58 -0700

    bug fix for mobile menu

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -35,12 +35,12 @@ class Header extends React.Component {
                         </div>
                         <div className="bg-gray-900 text-center">
                             <Menu right isOpen={this.state.menuOpen}
-                                onStateChange={(state) => this.handleStateChange(state)} width={'100%'} >
-                                <div className="gird grid-cols-1 justify-center">
-                                    <div className="p-20">
-                                        <img className="inline-block align-middle h-48 rounded-full" src="/assets/images/profile.jpg" alt="Matthew Evans" />
-                                        <h2 className="text-xl lg:text-3xl font-extrabold dark:text-gray-300 pt-5">Matt Evans</h2>
-                                        <h3 className="text-xl lg:text-xl dark:text-gray-300 pt-1">Full Stack & Mobile Application Developer</h3>
+                                onStateChange={(state) => this.handleStateChange(state)} width={'100%'} pageWrapId={"page-wrap"}>
+                                <main id="page-wrap">
+                                    <div className="">
+                                        <img className="inline-block align-middle h-24 md:h-15 rounded-full" src="/assets/images/profile.jpg" alt="Matthew Evans" />
+                                        <h2 className="text-xl sm:text-3xl font-extrabold dark:text-gray-300 pt-5">Matt Evans</h2>
+                                        <h3 className="text-xl lg:text-xl dark:text-gray-300 pt-1 pb-20">Full Stack & Mobile Application Developer</h3>
                                     </div>
                                     <div className="p-2">
                                         <NavLink onClick={() => this.closeMenu()} className="text-4xl md:text-4xl font-bold uppercase text-gray-300 hover:text-gray-500 dark:text-gray-300 dark:hover:text-gray-500" to="/">
@@ -62,18 +62,19 @@ class Header extends React.Component {
                                             <span>Contact</span>
                                         </NavLink>
                                     </div>
-                                </div>
-                                <div className="social-links">
-                                    <a className="btn btn-social-icon px-4 text-xl" href="http://instagram.com/mattevansdev">
-                                        <i className="fa fa-instagram text-gray-300 hover:text-blue-400"></i>
-                                    </a>
-                                    <a className="btn btn-social-icon px-4 text-xl" href="http://facebook.com/matthew.evans">
-                                        <i className="fa fa-facebook text-gray-300 hover:text-blue-400"></i>
-                                    </a>
-                                    <a className="btn btn-social-icon px-4 text-xl" href="http://twitter.com/matthewevansdev">
-                                        <i className="fa fa-twitter text-gray-300 hover:text-blue-400"></i>
-                                    </a>
-                                </div>
+
+                                    <div className="social-links">
+                                        <a className="btn btn-social-icon px-4 text-xl" href="http://instagram.com/mattevansdev">
+                                            <i className="fa fa-instagram text-gray-300 hover:text-blue-400"></i>
+                                        </a>
+                                        <a className="btn btn-social-icon px-4 text-xl" href="http://facebook.com/matthew.evans">
+                                            <i className="fa fa-facebook text-gray-300 hover:text-blue-400"></i>
+                                        </a>
+                                        <a className="btn btn-social-icon px-4 text-xl" href="http://twitter.com/matthewevansdev">
+                                            <i className="fa fa-twitter text-gray-300 hover:text-blue-400"></i>
+                                        </a>
+                                    </div>
+                                </main>
                             </Menu>
                         </div>
                     </div>
